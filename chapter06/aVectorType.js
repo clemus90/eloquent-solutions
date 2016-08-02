b8a3ae52a43c5b89113e71fd111f617abc60bc32
Christian Lemus <crlg90@gmail.com>
2016-08-02 12:01:56 -0500

    solves first exercise from Chapter 6

---
+++ chapter06/aVectorType.js
@@ -0,0 +1,24 @@
+var Vector = function(x, y){
+  this.x = x;
+  this.y = y;
+};
+
+Vector.prototype.plus = function(vec){
+  return new Vector(this.x + vec.x, this.y + vec.y);
+};
+Vector.prototype.minus = function(vec){
+  return new Vector(this.x - vec.x, this.y - vec.y);
+};
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function(){
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+});
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(new Vector(1, 2).minus(new Vector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(new Vector(3, 4).length);
+// → 5
