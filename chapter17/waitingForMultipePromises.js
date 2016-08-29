736c9a33a2bcb43fbd9bb3f65f8558a8e6ee0a51
Christian Lemus <crlg90@gmail.com>
2016-08-29 10:14:57 -0500

    finishes Chapter 17 exercises

---
+++ chapter17/waitingForMultipePromises.js
@@ -0,0 +1,48 @@
+var Promise = require('promise');
+
+function all(promises){
+  return new Promise(function(success, fail){
+    var result = [];
+    var successfulPromises = 0;
+    promises.forEach(function(promise, index){
+      promise.then(function(val){
+        result[index] = val;
+        successfulPromises ++;
+        if(successfulPromises == promises.length){
+          success(result);
+        }
+      },
+      function(err){
+        fail(err);
+      });
+    }); 
+    if(successfulPromises == promises.length){
+      success(result);
+    }
+  });
+}
+
+// Test code
+all([]).then(function(array) {
+  console.log("This should be []:", array);
+});
+function soon(val) {
+  return new Promise(function(success) {
+    setTimeout(function() { success(val); },
+               Math.random() * 500);
+  });
+}
+all([soon(1), soon(2), soon(3)]).then(function(array) {
+  console.log("This should be [1, 2, 3]:", array);
+});
+function fail() {
+  return new Promise(function(success, fail) {
+    fail(new Error("boom"));
+  });
+}
+all([soon(1), fail(), soon(3)]).then(function(array) {
+  console.log("We should not get here");
+}, function(error) {
+  if (error.message != "boom")
+    console.log("Unexpected failure:", error);
+});
